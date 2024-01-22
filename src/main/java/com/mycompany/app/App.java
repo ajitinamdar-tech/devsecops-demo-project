10869b2df11ef17ad2c48b3f498c898efcd1ef0e
Ajit Inamdar <ajitinamdar.tech@gmail.com>
2024-01-22 23:32:29 +0530

    .

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -18,11 +18,16 @@ public class App extends HttpServlet{
    public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
-      // Set response content type
-      response.setContentType("text/html");
+      
+      try {
+         // Set response content type
+         response.setContentType("text/html");
 
-      // Actual logic goes here.
-      PrintWriter out = response.getWriter();
-      out.println("<h1>" + message + "</h1>");
+         // Actual logic goes here.
+         PrintWriter out = response.getWriter();
+         out.println("<h1>" + message + "</h1>");
+      } catch (Exception e) {
+         System.out.println("Something went wrong.");
+      }
    }
 }
